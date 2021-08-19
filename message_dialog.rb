312fc920f76f97a5b08d763c8efa53e767dd2e87
asahi <asap1017@docomo.ne.jp>
2021-08-19 18:47:34 +0900

    attackメッセージの切り出し

---
+++ message_dialog.rb
@@ -0,0 +1,10 @@
+module MessageDialog
+
+  def attack_message(**params)
+    attack_type = params[:attack_type]
+
+    puts "#{@name}の攻撃"
+    puts "必殺攻撃" if attack_type == "special_attack"
+  end
+
+end
